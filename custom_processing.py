700033e27b3a7b80aae86458db24df40d6518518
Spiderweak <spiderweak@a-bernard.fr>
2023-11-14 07:14:48 -0500

    adds processing logic to custom processing file

---
+++ custom_processing.py
@@ -0,0 +1,74 @@
+# Import necessary libraries
+from typing import Any, Tuple
+import pandas as pd
+import re
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import pandas_udf
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+import logging
+
+def process_data():
+
+    # Initialize Spark session
+    spark = SparkSession.builder.appName("LlamaOracle").getOrCreate()
+
+    def llama2_answer_questions(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Apply function for Spark to call Llama model and get answers for questions.
+
+        Args:
+            df: Pandas DataFrame with columns 'question' and 'category'.
+
+        Returns:
+            DataFrame with the Llama model's responses.
+        """
+        from llama_cpp import Llama
+        import os
+
+        # Configuration: Model path should be an environment variable.
+        model_path = os.getenv("LLAMA_MODEL_PATH")
+
+        llm = Llama(model_path=model_path, n_ctx=8192, n_batch=512)
+
+        # template for this model version, see:
+        # https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML#prompt-template-llama-2-chat
+        template = """
+        [INST] <<SYS>>
+        You are a helpful, respectful and honest assistant. 
+        Always answer as helpfully as possible, while being safe.  
+        Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. 
+        Please ensure that your responses are socially unbiased and positive in nature.
+
+        If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
+        If you don't know the answer to a question, please don't share false information.
+        <</SYS>>
+
+        {INSERT_PROMPT_HERE} [/INST]
+        """
+
+        # Process each of the provided rows
+        answers = []
+        for index, row in df.iterrows():
+            question_text = row['question']
+            question_category = row['category']
+            prompt = 'Please answer the following questions in a single sentence (less than 100 words): ' + question_text
+            prompt = template.replace('{INSERT_PROMPT_HERE}', prompt)
+
+            try:
+                output = llm(prompt, max_tokens=2048, echo=False)
+                answer_text = output['choices'][0]['text']
+                print(answer_text)
+            except Exception as e:
+                # Log the error and return an empty DataFrame or handle it as needed.
+                logging.error(f"Error when calling Llama model: {e}")
+                answer_text = "Error in processing"
+            # Append the answer and category to the answers list
+            answers.append((answer_text, question_category))
+
+        # Return the DataFrame with answers.
+        return pd.DataFrame(
+            answers,
+            columns=['answer', 'category']
+        )
